fda8e7d5fef3186b7771648cb77ea102000bc396
Alex Saltzman <a.saltzman920@gmail.com>
2021-06-27 16:06:30 -0500

    add tests for checking proper runcontainer file bookkeeping

--- src/mspcrunner/containers.py
+++ src/mspcrunner/containers.py
@@ -24,6 +24,7 @@ class RunContainer:
         self._files = list()
         self._file_mappings = dict()
         self._rootdir = rootdir
+        self._files_added = 0
         # self._spectra = None
         # self._pinfile = None
         # self._tsv_searchres = None
@@ -32,6 +33,11 @@ class RunContainer:
         # self._mokapot_peptides = None
         # self._sics = None
         # self._reporterions = None
+
+    def reset_properties(self):
+        self._stem = None
+        # print('reste', self._stem)
+        self._rootdir = None
 
     @property
     def spectra(self):
@@ -46,12 +52,22 @@ class RunContainer:
     @property
     def stem(self):
         if self._stem is None and self._files:
-            stem_length = min(len(x.stem) for x in self._files)
-            _stem = self._files[0].name[0:stem_length]
-            stems = {x.stem for x in self._files}
+
+            name = ""
+            for ix, char in enumerate(self._files[0].stem):
+                if all(
+                    self._files[x].name[ix] == char for x in range(len(self._files))
+                ):
+                    name += char
+                else:  # stop once we've found a difference
+                    break
+            _stem = name.strip("_")
+
+            # stem_length = min(len(x.stem) for x in self._files)
+            # _stem = self._files[0].name[0:stem_length]
+            # stems = {x.stem for x in self._files}
             # if len(stems) > 1:
-            #    raise ValueError('!!')
-            # self._stem = tuple(stems)[0]
+            #   raise ValueError('!!')
             self._stem = _stem
         return self._stem
 
@@ -68,8 +84,8 @@ class RunContainer:
     def add_file(self, f):
         # keep a record of all files
 
-
         # always store raw files in "raw"
+        print(f)
         if f.name.endswith("raw"):
             self._file_mappings["raw"] = f
 
@@ -99,6 +115,7 @@ class RunContainer:
             return
             # logger.info(f"Unknown file {f}")
         self._files.append(f)
+        self.reset_properties()
 
     def update_files(self) -> None:
         """"""
@@ -112,22 +129,40 @@ class RunContainer:
         # Can add more logic such as checking if file exists, file size, creation time, etc
         self.update_files()
 
+        # if name in ("raw", "spectra"):
+        #     if self._file_mappings['raw'].name() == self._file_mappings['spectra'].name():
+        #         self._file_mappings['raw'] = self._file_mappings['spectra'].resolve()
         return self._file_mappings.get(name)
 
         # return self.attrs.get(name, lambda x: x)()
 
     def relocate(self, new_dir: Path):
-        # doesn't actually relocate. this is in FileMover
-        # should probably combine here
 
-        for filetype, file in self._file_mappings.items():
+        for filetype, fileref in self._file_mappings.items():
+            file = self.get_file(filetype)
+
             if not isinstance(file, Path):
                 continue
 
             new_file = new_dir / file.parts[-1]
 
+            print("===*** ", filetype, file.resolve(), new_file.resolve())
+
             if not file.resolve() == new_file.resolve():
                 logger.info(f"{file} -> {new_file}")
                 relocated_obj = file.rename(new_file)
                 self._file_mappings[filetype] = relocated_obj
-            # file = self.get_file(filetype)
+
+            if filetype in ("raw", "spectra"):
+                if (
+                    self._file_mappings["raw"].suffix
+                    == self._file_mappings["spectra"].suffix
+                ):
+                    self._file_mappings["raw"] = self._file_mappings[
+                        "spectra"
+                    ] = new_file
+
+        # if filetype in ("raw", "spectra"):
+        #     self._file_mappings['raw'] = new_file
+
+        # file = self.get_file(filetype)
--- src/tests/unittests/test_runcontainer.py
+++ src/tests/unittests/test_runcontainer.py
@@ -1,3 +1,4 @@
+# TODO use fixtures?
 from pathlib import Path
 
 import pytest
@@ -5,6 +6,12 @@ import pytest
 import mspcrunner
 from mspcrunner import containers
 from mspcrunner.containers import RunContainer
+
+
+@pytest.fixture
+def two_valid_files():
+    rawfile = Path("testfile.raw")
+    mzmlfile = Path("testfile.mzML")
 
 
 def test_stem_default_none():
@@ -26,8 +33,6 @@ def test_add_spectrafile():
     assert run_container.stem is not None
 
 
-
-
 def test_add_raw_then_mzml():
     run_container = RunContainer()
 
@@ -44,32 +49,71 @@ def test_add_raw_then_mzml():
     assert run_container.stem is not None
 
 
-def test_stem_add_irrelevant_filexxx():
-    run_container = RunContainer(stem=None)
-    testfiles = [
-        "12345_1_1_aofejf",
-        "12345_1_1_aofejf",
-        "12345_1_1_aofejf",
-        "12345_1_1_aofejf",
-    ]
-    # run_container[]
-    assert run_container.stem is None
+def test_property_reset():
 
-def test_rootdir_set(tmp_path):
-    f1 = tmp_path / 'testfile.raw'
-    f2 = tmp_path / 'testfile.tsv'
+    f1 = Path("12345_1_1_1file.raw")
+    f2 = Path("12345_1_1_2file.mzML")
+
+    run_container = RunContainer(stem=None)
+    run_container.add_file(f1)
+    assert run_container.stem == f1.stem
+
+    run_container.add_file(f2)
+    assert run_container._stem is None
+
+
+def test_stem_creation():
+    run_container = RunContainer(stem=None)
+
+    f1 = Path("12345_1_1_1file.raw")
+    f2 = Path("12345_1_1_2file.mzML")
+
+    run_container.add_file(f1)
+    run_container.add_file(f2)
+
+    print(run_container.stem)
+    assert run_container.stem == "12345_1_1"
+
+    run_container.add_file(Path("irrelevant-file"))
+    assert run_container.stem == "12345_1_1"
+
+
+def test_rootdir_set(
+    tmp_path,
+):
+    f1 = tmp_path / "testfile.raw"
+    f2 = tmp_path / "testfile.tsv"
 
     run_container = RunContainer()
     run_container.add_file(f1)
     run_container.add_file(f2)
     assert run_container.rootdir == tmp_path
 
+
+def test_relocate(tmp_path):
+    d = tmp_path / "old"
+    d.mkdir()
+    f1 = d / "testfile.raw"
+    f1.touch()
+    assert f1.exists()
+    assert d.exists()
+
+    run_container = RunContainer()
+    run_container.add_file(f1)
+
+    dnew = tmp_path / "new"
+    dnew.mkdir()
+    assert dnew.exists()
+    print(dnew, list(dnew.glob("*")))
+    run_container.relocate(dnew)
+
+
 def test_rootdir_valueerror(tmp_path):
-    f1 = tmp_path / 'testfile.raw'
-    f2 = tmp_path / 'loc' /'testfile.tsv'
+    f1 = tmp_path / "testfile.raw"
+    f2 = tmp_path / "loc" / "testfile.tsv"
 
     run_container = RunContainer()
     run_container.add_file(f1)
     run_container.add_file(f2)
     with pytest.raises(ValueError):
-         run_container.rootdir
+        run_container.rootdir
