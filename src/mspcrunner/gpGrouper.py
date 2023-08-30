106ad8287e00366c7204227a52d2603b49f3e6c7
Alex Saltzman <saltzman@bcm.edu>
2023-08-30 12:42:52 -0500

    try to fix incorrect gpG container creation

--- src/mspcrunner/gpGrouper.py
+++ src/mspcrunner/gpGrouper.py
@@ -15,15 +15,20 @@ from .logger import get_logger
 
 logger = get_logger(__name__)
 
+##    "15 17 19",
+# _ion_score_bins = [16, 18, 20]
+# _ion_score_bins = [15, 17, 19]
+#
+# _ion_score_bins = "15 17 19"
 
 BASE = [
     "gpgrouper",
     "run",
-    "--ion-score-bins",
-    "16 18 20",
+    # "--ion-score-bins",
+    # _ion_score_bins,
     "-s",
     "./params/gpgrouper.conf",
-    #Predefined_gpG.default,
+    # Predefined_gpG.default,
     "--taxonid",
     "9606",
     "--workers",
@@ -43,6 +48,7 @@ class gpGrouper(Command):
         name=None,
         refseq=None,
         workers=1,
+        force=False,  # if true will run even if output already exists
         runcontainer=None,
         sampleruncontainer=None,
         runcontainers=None,  # either pass 1 run
@@ -55,6 +61,7 @@ class gpGrouper(Command):
             paramfile=paramfile,
             name=name,
             refseq=refseq,
+            force=force,
             **kwargs,
         )
         # super().__init__(
@@ -65,6 +72,7 @@ class gpGrouper(Command):
         #     name=name,
         #     **kwargs,
         # )
+        logger.info(f"gpGrouper {self} __init__")
         self.workers = workers
         self.phospho = kwargs.get("phospho", False)
         self.no_taxa_redistrib = kwargs.get("no_taxa_redistrib", False)
@@ -76,16 +84,51 @@ class gpGrouper(Command):
         self.paramfile = kwargs.get("paramfile", Predefined_gpG.default.value)
         self.sampleruncontainer = sampleruncontainer
         self.sampleruncontainers = sampleruncontainers
+        import ipdb
+
         # self.record_no = kwargs.get("record_no", "none")
         # self.run_no = kwargs.get("run_no", "none")
         # self.search_no = kwargs.get("search_no", "none")
 
-    def create(self, sampleruncontainers=None, **kws):
+    def create(self, sampleruncontainers=None, **kwargs):
+
+        if "force" in kwargs:
+            force = kwargs.pop("force")
+        else:
+            force = self.force
+        res = super().create(sampleruncontainers=sampleruncontainers, **kwargs)
         if sampleruncontainers is None:
-            return
-            logger.error(f"!!")
+            sampleruncontainers = self.sampleruncontainers
+
+        logger.info(f"gpGrouper {self} create")
+        if sampleruncontainers is None:
+            logger.error("No sampleruncontainers passed")
             # `raise ValueError(f"Must pass an iterable of SampleRunContainers")
+
+        # set([x.rec_run_search for x in sampleruncontainers])
+
+        _done = set()
+        # import ipdb; ipdb.set_trace()
+
         for ix, samplerun_container in enumerate(sampleruncontainers):
+            if samplerun_container.psms_filePath is None:
+                logger.info(f"skipping {samplerun_container} because no psms file")
+                continue
+            if (
+                samplerun_container.get_file("e2g_QUAL") is not None
+                and bool(force) == False
+            ):
+                logger.info(
+                    f"Found e2g_QUAL file for {samplerun_container}, not regrouping"
+                )
+                continue
+            # if samplerun_container.rec_run_search == "52042_1_9":
+            #    import ipdb
+
+            #    ipdb.set_trace()
+            #    1 + 1
+            if samplerun_container.rec_run_search in _done:  # keeps track above
+                continue
             kws = self.__dict__.copy()
             # kws["inputfiles"] = samplerun_container
             kws["sampleruncontainer"] = samplerun_container
@@ -93,6 +136,8 @@ class gpGrouper(Command):
             kws["receiver"] = kws["_receiver"]
             # set_trace()
             yield gpGrouper(**kws)
+
+            _done.add(samplerun_container.rec_run_search)
 
     @property
     def CMD(self):
@@ -106,6 +151,7 @@ class gpGrouper(Command):
             return
         sampleruncontainer = self.sampleruncontainer
         psms_file = sampleruncontainer.psms_filePath
+        # import ipdb; ipdb.set_trace()
         if psms_file is None:
             return  # not good
 
@@ -115,7 +161,7 @@ class gpGrouper(Command):
         run_no = sampleruncontainer.run_no
         search_no = sampleruncontainer.search_no
         # psms_file = samplerun_container.psms_filePath
-        _ion_score_bins = [16, 18, 20]
+        _ion_score_bins = [8, 10, 13]
         if search_no == "1":
             _ion_score_bins = [10, 20, 30]
 
