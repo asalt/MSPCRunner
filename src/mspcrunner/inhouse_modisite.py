a7f9f8718064f7cee7bfc7abb503d00242deb666
Alex Saltzman <saltzman@bcm.edu>
2024-03-07 13:08:45 -0600

    remove create overwrite

--- src/mspcrunner/inhouse_modisite.py
+++ src/mspcrunner/inhouse_modisite.py
@@ -20,7 +20,8 @@ BASEDIR = os.path.split(__file__)[0]
 class AnnotateSite(Command):
 
     NAME = "AnnotateSite"
-    ANNOTATE_SCRIPT = BASEDIR / Path("ext/annotate_site/annotate_protein.py")
+    # ANNOTATE_SCRIPT = BASEDIR / Path("ext/annotate_site/annotate_protein.py")
+    ANNOTATE_SCRIPT = BASEDIR / Path("sitequant/main.py")
 
     def __init__(
         self,
@@ -31,6 +32,7 @@ class AnnotateSite(Command):
         inputfiles=None,
         container=None,
         containers=None,
+        sampleruncontainers=None, # we use this one
         refseq=None,
         **kwargs,
     ):
@@ -52,24 +54,26 @@ class AnnotateSite(Command):
         self.workers = workers
         self.refseq = refseq
         self.containers = containers  # are we using this? No?
+        self.sampleruncontainers = sampleruncontainers  # are we using this? No?
+        logger.info(f"AnnotateSite: {self.__dict__}")
 
-    def create(self, sampleruncontainers=None, **kwargs):
-        if self.containers is not None and sampleruncontainers is not None:  # add
-            sampleruncontainers = sampleruncontainers + list(
-                filter(None, self.containers)
-            )
-        elif sampleruncontainers is None and self.containers:
-            sampleruncontainers = self.containers
+    # def create(self, sampleruncontainers=None, **kwargs):
+    #     if self.containers is not None and sampleruncontainers is not None:  # add
+    #         sampleruncontainers = sampleruncontainers + list(
+    #             filter(None, self.containers)
+    #         )
+    #     elif sampleruncontainers is None and self.containers:
+    #         sampleruncontainers = self.containers
 
-        if sampleruncontainers is None:
-            yield self
+    #     if sampleruncontainers is None:
+    #         yield self
 
-        d = self.__dict__.copy()
-        for kw in kwargs:
-            if kw in d:
-                d.update(kw, kwargs[kw])
-        d["containers"] = sampleruncontainers
-        yield type(self)(**d)
+    #     d = self.__dict__.copy()
+    #     for kw in kwargs:
+    #         if kw in d:
+    #             d.update(kw, kwargs[kw])
+    #     d["containers"] = sampleruncontainers
+    #     yield type(self)(**d)
 
     @property
     def CMD(self):
@@ -77,23 +81,25 @@ class AnnotateSite(Command):
         if self.containers is None:
             return
 
-        self.containers
         ncores = self.workers
 
-        expids = [x.rec_run_search for x in self.containers]
+        expids = [x.rec_run_search for x in self.sampleruncontainers]
         # "psm_qual"
-        psm_files = filter(None, [x.get_file("psm_QUANT") for x in self.containers])
-        # import ipdb
-
-        # ipdb.set_trace()
+        # _target = "psms_QUANT"
+        # psm_files = list(filter(None, [x.get_file(_target) for x in self.containers]))
+        # if len(psm_files) == 0:
+        #     logger.error(
+        #         f"No psms files found for mapping {_target} for {self.containers}"
+        #     )
+        #     return
 
         cmd = [
             f"python",
             self.ANNOTATE_SCRIPT,
-            "--combine",
-            "--all-genes",
-            "--noplot",
-            "--nr",
+            # "--combine",
+            # "--all-genes",
+            # "--noplot",
+            # "--nr",
             "--cores",
             ncores,
             "--fasta",
@@ -102,9 +108,11 @@ class AnnotateSite(Command):
             # OUTNAME,
         ]
         # for psm_file in psm_files:
-        for psm_file in expids:
+        # for psm_file in psm_files:
+        for expid in expids:  # load with bcmproteomics
+            logger.info(f"Adding {expid} to {self}")
             cmd.append("--psms")
-            cmd.append(psm_file)
+            cmd.append(expid)
         # import ipdb
 
         # ipdb.set_trace()
